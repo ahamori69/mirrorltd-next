b2fe3ceffabbf3c1fdda489897c28711487c31af
ahamori69 <132799340+ahamori69@users.noreply.github.com>
2023-05-07 10:29:25 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          Developed by
+          <code className={styles.code}>Toufiq Hasan Kiron</code>
         </p>
         <div>
           <a
